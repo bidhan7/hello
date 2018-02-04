e39cb9671d6585c7a166544022d13c881de4f77d
bidhan7 <bidhan.subedi7@gmail.com>
2018-02-03 19:34:44 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,6 +6,6 @@ int main(){
 	printf("SUM (a+b)=%f\n",sum);
 	printf("DIFFERENCE (a-b)= %f\n",difference);
 	printf("Hello world. My name is Bishal.");
-	printf("Hello Pudke");
+	printf("Hello Gade");
 	return 0;
 }
