1677e6ece41ed2bd4f487bdf900aea3b9847a73a
bidhan.subedi7@gmail.com <bidhan.subedi7@gmail.com>
2018-02-03 19:01:54 -0600

    first commit

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-	float a=3,b=5,sum=0,difference=0;
+	float a=3,b=5,sum,difference;
 	sum= a+b;
 	difference=a-b;
-	printf("SUM (a+b)=" "%f",sum);
-	printf("DIFFERENCE (a-b)=" "%f",difference);
+	printf("SUM (a+b)=%f\n",sum);
+	printf("DIFFERENCE (a-b)= %f\n",difference);
 	printf("Hello world. My name is Bishal.");
 	return 0;
 }
